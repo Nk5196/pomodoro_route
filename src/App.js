e70027ed1dd2dba37adada9ff8d2e0cc27edffe8
Nk5196 <narendrakandalkar@gmail.com>
2023-08-10 03:20:13 +0530

    auth

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Component/Header';
-import { ChakraProvider, Box } from '@chakra-ui/react'
 import Body from './Component/Body';
-
+import Login from './Component/Login';
+import Signup from './Component/Signup'; // Import the Signup component
+import ProtectedRoute from './Component/Protected_Route';
+import { Box } from '@chakra-ui/react';
 
 function App() {
   return (
-    <ChakraProvider>
-    <Box className="App" w='100%'  h='100vh' bgGradient='linear(to-l, #000103, #07345b)'>    
-       <Header/>
-       <Body/> 
+    <Box  w='100%'  h='100vh' bgGradient='linear(to-l, #000103, #07345b)' color='white'>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<ProtectedRoute />}>
+          <Route index element={<Body />} />
+        </Route>
+      </Routes>
+    </Router>
     </Box>
-    </ChakraProvider>
   );
 }
 
--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,22 +1,33 @@
 import React from 'react'
-import { Heading, Flex, Spacer, Box } from '@chakra-ui/react'
+import { Heading, Flex, Spacer, Box, Show } from '@chakra-ui/react'
 
 import { BiSolidUser } from 'react-icons/bi';
-import { IoTimer } from 'react-icons/io5';
+
 const Header = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            await auth.signOut();
+            console.log('Logged out successfully');
+            navigate('/login');
+        } catch (error) {
+            console.error('Logout error', error);
+        }
+    };
+
     return (
-        <Box >
-            <Flex p={4} color={'white'} >
-                <Box width='100px'><IoTimer  size='30px'/> </Box><Spacer/>
+        <Box>
+            <Flex p={4} color={'white'}>
+                <Box><IoTimer size='30px' /></Box><Spacer />
                 <Heading as='h4' size='md' fontWeight={'semibold'}>
                     Pomodoro
                 </Heading>
                 <Spacer />
-                <Flex width='100px' > <Box mt={'0px'}  mr={2} p={1}><BiSolidUser size='20px'/> </Box> <Spacer/><Heading size='md'  fontWeight={'semibold'} > Login</Heading></Flex>
+                <Flex> <Box mt={'0px'} mr={2} p={1}><BiSolidUser size='20px' /> </Box> <Spacer /><Show above='md'><Heading size='md' fontWeight={'semibold'} > Login</Heading></Show></Flex>
             </Flex>
         </Box>
+    );
+};
 
-    )
-}
-
-export default Header
+export default Header;
